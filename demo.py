0a2e5b1a8fbc3ad32ed2081ce31c1f36659ee456
ac125689 <atirumala844@gmail.com>
2024-03-29 11:44:19 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -14,33 +14,37 @@ usr = input()
 #for i in range(len(usr)):
 #    print(f"{usr[i]} = {ord(usr[i])}")
 
-
-def password_Strenght_Validtion(password):
-    spCha = 0
-    upAphlCha = 0
-    loAphlCha = 0
-    print(len(password))
-    if (len(password)<4) or (len(password)>26):
-        print("INVALID: Password is to long/shrot password should be in between 4 - 26 Characters")
-    else:
-        print("VALID: Password is in between 4 - 26 Characters")
-        for i in range(len(password)):
-            if ((ord(password[i])>=32) and (ord(password[i])<=64)) or ((ord(password[i])>=91) and (ord(password[i])<=96)) or ((ord(password[i])>=123) and (ord(password[i])<=126)):
-                spCha +=1
-            elif ((ord(password[i])>=65) and (ord(password[i])<=90)):
-                upAphlCha +=1
-            else:
-               loAphlCha +=1
-
-        if  (spCha < 2) or (spCha > 26):
-            print("INVALID: Password as to many/little special Characters it should be in between 2 - 26 Characters.")
-        else:
-            print("VALID: Password special Characters are in between 2 - 26 Characters.")
+class ClassUser():
+    def __init__(Username,Password):
+        self.Username = Username
+        self.Password = Password
         
-        if (upAphlCha >=1) and (loAphlCha<=1):
-            print("This password is strong.")
+    def password_Strenght_Validtion():
+        spCha = 0
+        upAphlCha = 0
+        loAphlCha = 0
+        print(len(self.Password))
+        if (len(self.Password)<4) or (len(self.Password)>26):
+            print("INVALID: Password is to long/shrot password should be in between 4 - 26 Characters")
         else:
-            print("INVALID: Password as to need at least one upper/lower Characters")
+            print("VALID: Password is in between 4 - 26 Characters")
+            for i in range(len(self.Password)):
+                if ((ord(self.Password[i])>=32) and (ord(self.Password[i])<=64)) or ((ord(self.Password[i])>=91) and (ord(self.Password[i])<=96)) or ((ord(self.Password[i])>=123) and (ord(self.Password[i])<=126)):
+                    spCha +=1
+                elif ((ord(self.Password[i])>=65) and (ord(self.Password[i])<=90)):
+                    upAphlCha +=1
+                else:
+                    loAphlCha +=1
+
+            if  (spCha < 2) or (spCha > 26):
+                print("INVALID: Password as to many/little special Characters it should be in between 2 - 26 Characters.")
+            else:
+                print("VALID: Password special Characters are in between 2 - 26 Characters.")
+            
+            if (upAphlCha >=1) and (loAphlCha<=1):
+                print("This password is strong.")
+            else:
+                print("INVALID: Password as to need at least one upper/lower Characters")
 
 
         
