957e069550a5472b713a1e15d5afd5aa29d6facb
ac125689 <atirumala844@gmail.com>
2024-03-29 14:59:03 -0400

    Update fileReader.py

--- fileReader.py
+++ fileReader.py
@@ -7,20 +7,3 @@ def read_Userfile(fileName):
 
 name = "test.txt"
 read_Userfile(name)
-
-#import aes_cipher
-
-#Pbkdf2Sha512Default = aes_cipher.Pbkdf2Sha512(512 * 1024)
-#data = "asfdfasdggasdffasDas"
-#hi=aes_cipher.DataEncrypter(Pbkdf2Sha512Default)
-#hi.Encrypt(data,"test_pwd")
-
-#print(hi.GetEncryptedData())
-
-
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-f = Fernet(key)
-token = f.encrypt(b"A really secret message. Not for prying eyes.")
-
-print(token)
