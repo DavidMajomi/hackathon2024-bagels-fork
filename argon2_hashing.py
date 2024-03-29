1522920f32f1d4f532f6c79b3fdf68662665b0f2
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-03-29 14:52:18 -0400

    Update argon2_hashing.py

--- argon2_hashing.py
+++ argon2_hashing.py
@@ -4,8 +4,12 @@ import argon2
 def get_user_passphrase():
     user_entered_passphrase = input("Enter the passphrase: ")
     user_entered_passphrase = bytes(user_entered_passphrase, 'utf-8')
-    hashed_passphrased = argon2.hash_password(user_entered_passphrase)
-    print(hashed_passphrased)
+    
+    hashed_passphrase = argon2.hash_password(user_entered_passphrase)
+    print(hashed_passphrase)
+    
+    return hashed_passphrase
+    
     
 
 get_user_passphrase()
