3138efe4038126612d72c550501e7365c788dbc3
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-03-29 15:53:01 -0400

    Update argon2_hashing.py
    Switched to fernet hashing

--- argon2_hashing.py
+++ argon2_hashing.py
@@ -1,14 +1,27 @@
-import argon2
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def get_user_passphrase():
     user_entered_passphrase = input("Enter the passphrase: ")
     user_entered_passphrase = bytes(user_entered_passphrase, 'utf-8')      # Converts passphrase to bytes
+
+    salt = os.urandom(16)
     
-    hashed_passphrase = argon2.hash_password(user_entered_passphrase)      # Encrypt passphrase using argon2
-    # print(hashed_passphrase)
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=480000,
+    )
+    
+    hashed_passphrase = base64.urlsafe_b64encode(kdf.derive(user_entered_passphrase))
+    print(hashed_passphrase)
+    print("Hey")
     
     return hashed_passphrase
     
     
-
 get_user_passphrase()
