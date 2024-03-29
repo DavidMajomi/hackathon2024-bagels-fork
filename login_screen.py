5b58d6a67e32317a72bd79aa906edb5a450dca36
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-03-29 13:05:54 -0400

    Update login_screen.py

--- login_screen.py
+++ login_screen.py
@@ -18,7 +18,7 @@ def login():
         
         # if()
         
-        user_data = return_row_associated_with_user()
+        user_data = return_row_associated_with_user(user_entered_password)
         passwords = parse_returned_passwords(user_data)
         
         for stored_password in passwords:
@@ -27,6 +27,7 @@ def login():
                 os.system("cls")
                 print(f"Welcome {user_name}")
             else:
+                print('Invalid check your username and password')
                 number_of_user_tries = number_of_user_tries + 1
                 os.system("cls")
                 
