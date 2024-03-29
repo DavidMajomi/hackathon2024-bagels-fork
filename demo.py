cf76b897d69f0a1a571de8557bf5228972cd6fcf
ac125689 <atirumala844@gmail.com>
2024-03-29 13:02:34 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,45 +6,95 @@
 #91 - 96
 #123 - 126
 
-#num
-#48 - 57
-usr = input()
-
-
-#for i in range(len(usr)):
-#    print(f"{usr[i]} = {ord(usr[i])}")
 
 class ClassUser():
     def __init__(self, Username,Password):
         self.username = Username
         self.password = Password
+        self.spCha = 0
+        self.upAphlCha = 0
+        self.loAphlCha = 0
+
+
+    def get_upper(self):
+        return self.upAphlCha
+    
+    def get_lower(self):
+        return self.loAphlCha
+
     def getUsername(self):
         return self.username
+    
     def getPassword(self):
         return self.password
+    
     def password_Strenght_Validtion(self):
-        spCha = 0
-        upAphlCha = 0
-        loAphlCha = 0
+        
         if (len(self.password)<4) or (len(self.password)>26):
             print("INVALID: Password is to long/shrot password should be in between 4 - 26 Characters")
         else:
             print("VALID: Password is in between 4 - 26 Characters")
             for i in range(len(self.password)):
                 if ((ord(self.password[i])>=32) and (ord(self.password[i])<=64)) or ((ord(self.password[i])>=91) and (ord(self.password[i])<=96)) or ((ord(self.password[i])>=123) and (ord(self.password[i])<=126)):
-                    spCha +=1
+                    self.spCha +=1
                 elif ((ord(self.password[i])>=65) and (ord(self.password[i])<=90)):
-                    upAphlCha +=1
-                else:
-                    loAphlCha +=1
+                    self.upAphlCha +=1
+                elif((ord(self.password[i])>=87) and (ord(self.password[i])<=122)):
+                    self.loAphlCha +=1
 
-            if  (spCha < 2) or (spCha > 26):
+            if  (self.spCha < 2) or (self.spCha > 26):
                 print("INVALID: Password as to many/little special Characters it should be in between 2 - 26 Characters.")
             else:
                 print("VALID: Password special Characters are in between 2 - 26 Characters.")
             
-            if (upAphlCha >=1) and (loAphlCha<=1):
+            if (self.upAphlCha >=1) and (self.loAphlCha>=1):
                 print("This password is strong.")
             else:
                 print("INVALID: Password as to need at least one upper/lower Characters")
+
+    
+    def test_crack(self):
+
+        
+        #Mixed cases but no mixed characters
+        #65 - 122
+        if self.upAphlCha > 0 and self.loAphlCha > 0 and self.spCha == 0:
+            return ((122-65)**len(self.password))/5 
+
+        #No mixed cases just mixed_characters 
+        #32 - 64
+        elif self.upAphlCha > 0 and self.loAphlCha == 0 and self.spCha > 0:
+            return ((64-32)**len(self.password))/5
+        
+        #No mixed cases just mixed_characters 
+        #32 - 64
+        elif self.upAphlCha == 0 and self.loAphlCha > 0 and self.spCha > 0:
+            return ((64-32)**len(self.password))/5
+
+
+        #No mixed cases just mixed_characters 
+        #32 - 64
+        elif self.spCha == 0:
+            return ((64-32)**len(self.password))/5
+        
+        return (122**len(self.password))/5
+
+    
+#num
+#48 - 57
+print("Enter Username: ")
+usr = input()
+
+print("Enter Password")
+pssw = input()
+
+
+#for i in range(len(usr)):
+#    print(f"{usr[i]} = {ord(usr[i])}")
+
+user = ClassUser(usr, pssw)
+
+user.password_Strenght_Validtion()
+
+print(str(user.test_crack()) + " sec(s) to crack.")
 
