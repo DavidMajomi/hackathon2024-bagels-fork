40e86dd4b6248e0c9baa2c2749bc6c2cf5049840
ac125689 <atirumala844@gmail.com>
2024-03-29 15:43:02 -0400

    Update argon2_hashing.py

--- argon2_hashing.py
+++ argon2_hashing.py
@@ -1,11 +1,17 @@
-# from Crypto.cipher import AES
 import argon2
 
 def get_user_passphrase():
     user_entered_passphrase = input("Enter the passphrase: ")
+<<<<<<< HEAD
     user_entered_passphrase = bytes(user_entered_passphrase, 'utf-32')
     
     hashed_passphrase = argon2.hash_password(user_entered_passphrase)
+=======
+    user_entered_passphrase = bytes(user_entered_passphrase, 'utf-8')      # Converts passphrase to bytes
+    
+    hashed_passphrase = argon2.hash_password(user_entered_passphrase)      # Encrypt passphrase using argon2
+    # print(hashed_passphrase)
+>>>>>>> 5ca62c314beb6c50691a6ade549952f94fae1a5c
     
     return hashed_passphrase
 
