7bb4310b96e30ebb0729b487a19e301aa5100e58
ac125689 <atirumala844@gmail.com>
2024-03-29 15:41:36 -0400

    d

--- fileReader.py
+++ fileReader.py
@@ -1,9 +1,36 @@
-
+from cryptography.fernet import Fernet
+from argon2_hashing import get_user_passphrase
 def read_Userfile(fileName):
     file_name = open(fileName,'r')
     file_contenet = file_name.readlines()
     file_name.close()
+
     print(file_contenet)
+    passphrase = get_user_passphrase()
+    print(passphrase_64)
+    for i in range(len(file_contenet)):
+        f = Fernet(passphrase_64)
+
+        message = file_contenet[i].strip("\n")
+        token = f.encrypt(message)
+        print(token)
 
 name = "test.txt"
 read_Userfile(name)
+
+#import aes_cipher
+
+#Pbkdf2Sha512Default = aes_cipher.Pbkdf2Sha512(512 * 1024)
+#data = "asfdfasdggasdffasDas"
+#hi=aes_cipher.DataEncrypter(Pbkdf2Sha512Default)
+#hi.Encrypt(data,"test_pwd")
+
+#print(hi.GetEncryptedData())
+
+
+from cryptography.fernet import Fernet
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+
+print(token)
